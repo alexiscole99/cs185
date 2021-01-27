6f4fdc20eec947ac68b2d0616b7abb95d4fdf122
Alexis Cole <alexiscole@ucsb.edu>
2021-01-27 12:01:26 -0800

    video overlay

--- video.js
+++ video.js
@@ -13,16 +13,16 @@ function unhighlightHover(id) {
 // referenced https://www.w3schools.com/howto/howto_js_lightbox.asp
 // create "lightbox" on click
 
-function openModal(event, id) {
-    event.preventDefault();
-    console.log(event)
+function openModal(id) {
+    id = id.toString();
     console.log(id)
     var modal = document.getElementById("modal");
     var modalContent = document.getElementById("modal-content");
     var video = document.getElementById(id);
     var src = video.src;
     var width = "100%";
-    modalContent.innerHTML = "<iframe src="+ src +" width="+ width +">";
+    var height = "100%";
+    modalContent.innerHTML = "<iframe src="+ src +" width="+ width +" height="+ height +">";
     modal.style.display = "block";
 }
 
@@ -37,30 +37,3 @@ window.onclick = function(event) {
     }
 }
 
-/*document.getElementById("0").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("1").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("2").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("3").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("4").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("5").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("6").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("7").addEventListener("click", function(event) {
-    event.preventDefault();
-});
-document.getElementById("8").addEventListener("click", function(event) {
-    event.preventDefault();
-});*/
