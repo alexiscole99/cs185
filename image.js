c245ea9552508ffeb8865b0d1b388098997dc697
Alexis Cole <alexiscole@ucsb.edu>
2021-01-27 00:48:20 -0800

    almost

--- image.js
+++ image.js
@@ -9,3 +9,87 @@ function unhighlightHover(id) {
     var clickedTab = document.getElementById(id);
     clickedTab.setAttribute("class", "");
 }
+
+// display button
+function displayButton() {
+    var topButton = document.getElementById("top-button");
+    topButton.style.display = "inline-block";
+}
+
+// hide button
+function hideButton() {
+    var topButton = document.getElementById("top-button");
+    topButton.style.display = "none";
+}
+
+// return to top of page
+function toTop() {
+    document.body.scrollTop= 0;
+    hideButton();
+}
+
+// measure if scrolled past quarter of page
+// referenced http://www.javascriptkit.com/javatutors/detect-user-scroll-amount.shtml
+
+var windowHeight, documentHeight, trackLength, throttle;
+var percent = 0;
+// get window measurements
+function getMeasurements() {
+    windowHeight = window.innerHeight;
+    documentHeight = document.documentElement.scrollHeight;
+    trackLength = documentHeight - windowHeight;
+}
+
+// measure percentage scrolled
+function measureScroll() {
+    var scroll = window.pageYOffset;
+    percent = Math.floor(scroll/trackLength * 100);
+}
+
+getMeasurements();
+
+// get measurements every time the window resizes
+window.addEventListener("resize", getMeasurements);
+
+// add a throttled event listener to scroll
+window.addEventListener("scroll", function() {
+    clearTimeout(throttle);
+    throttle = setTimeout(function() {
+        measureScroll();
+        if(percent >= 25) {
+            // show button
+            displayButton();
+        } else {
+            // hide button
+            hideButton()
+        }
+    }, 50)
+});
+
+// referenced https://www.w3schools.com/howto/howto_js_lightbox.asp
+// create "lightbox" on click
+
+var portrait = [0,2,6,7,10,12,13];
+var landscape = [1,3,4,5,8,9,11,14,15,16];
+
+function openModal(id) {
+    var modal = document.getElementById("modal");
+    var modalContent = document.getElementById("modal-content");
+    var image = document.getElementById(id);
+    var src = image.src;
+    var width = "100%";
+    modalContent.innerHTML = "<img src="+ src +" width="+ width +">";
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
--- video.js
+++ video.js
@@ -9,3 +9,58 @@ function unhighlightHover(id) {
     var clickedTab = document.getElementById(id);
     clickedTab.setAttribute("class", "");
 }
+
+// referenced https://www.w3schools.com/howto/howto_js_lightbox.asp
+// create "lightbox" on click
+
+function openModal(event, id) {
+    event.preventDefault();
+    console.log(event)
+    console.log(id)
+    var modal = document.getElementById("modal");
+    var modalContent = document.getElementById("modal-content");
+    var video = document.getElementById(id);
+    var src = video.src;
+    var width = "100%";
+    modalContent.innerHTML = "<iframe src="+ src +" width="+ width +">";
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+/*document.getElementById("0").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("1").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("2").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("3").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("4").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("5").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("6").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("7").addEventListener("click", function(event) {
+    event.preventDefault();
+});
+document.getElementById("8").addEventListener("click", function(event) {
+    event.preventDefault();
+});*/
